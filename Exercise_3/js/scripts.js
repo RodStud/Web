8647ec35ea4f6708f0d31dc054785ba5c4244edb
RodStud <rodion.novakovskiy@stu.shenkar.ac.il>
2024-06-18 17:04:47 +0300

    Exercise 3 - Added JSON file with music

--- Exercise_3/js/scripts.js
+++ Exercise_3/js/scripts.js
@@ -15,22 +15,33 @@ window.onload = () => {
 };
 
 function initRectangles() {
+  document.getElementById("squares-container").style.display = "flex";
   for (let i = 0; i < person_name.length; i++) {
     addRectangle();
   }
 }
 
-function initSongs() {}
+function initSongs() {
+    document.getElementById("songs-container").style.display = "none";
+  fetch("data/music.json")
+    .then((response) => response.json())
+    .then((data) => {
+      for (let i = 0; i < data.length; i++) {
+        populateSongsInList();
+      }
+    })
+    .catch((error) => console.error("Error loading JSON:", error));
+}
 
 function initButton() {
   let header_buttons = document.getElementsByClassName("header-button");
   for (let i = 0; i < header_buttons.length; i++) {
     let button = header_buttons[i];
     button.addEventListener("mouseover", function () {
-      button.style.backgroundColor = "#a0a0a0";
+      button.style.backgroundColor = "#C0C0C0";
     });
     button.addEventListener("mouseout", function () {
-      button.style.backgroundColor = "#f0f0f0";
+      button.style.backgroundColor = "#FFFFFF";
     });
     switch (button.id) {
       case "add-button":
@@ -42,30 +53,44 @@ function initButton() {
       case "sub-button":
         button.addEventListener("click", subtractRectangle);
         break;
+      default:
+        break;
     }
   }
 }
 
-function chooseRectangleColor() {}
-
 function addRectangle() {
-  let squares_container = document.getElementById("squares-container");
-  let new_square = `<section class="square" style="background-color: ${colors[colors_index]}">${person_name[name_index]}</section>`;
-  squares_container.innerHTML += new_square;
-  nameIndexChange(1);
-  colorIndexChange(1);
-}
-
-function subtractRectangle() {
-  let squares_container = document.getElementsByClassName("square");
-  if (squares_container.length > 0) {
-    squares_container[squares_container.length - 1].remove();
-    nameIndexChange(-1);
-    colorIndexChange(-1);
+  if (document.getElementById("squares-container").style.display != "none") {
+    let squares_container = document.getElementById("squares-container");
+    let new_square = `<section class="square" style="background-color: ${colors[colors_index]}">${person_name[name_index]}</section>`;
+    squares_container.innerHTML += new_square;
+    nameIndexChange(1);
+    colorIndexChange(1);
   }
 }
 
-function switchRectanglesSongs() {}
+function subtractRectangle() {
+  if (document.getElementById("squares-container").style.display != "none") {
+    let squares_container = document.getElementsByClassName("square");
+    if (squares_container.length > 0) {
+      squares_container[squares_container.length - 1].remove();
+      nameIndexChange(-1);
+      colorIndexChange(-1);
+    }
+  }
+}
+
+function switchRectanglesSongs() {
+  let Rectangles = document.getElementById("squares-container");
+  let Songs = document.getElementById("songs-container");
+  if (Rectangles.style.display == "flex") {
+    Rectangles.style.display = "none";
+    Songs.style.display = "block";
+  } else {
+    Rectangles.style.display = "flex";
+    Songs.style.display = "none";
+  }
+}
 
 function populateSongsInList() {}
 

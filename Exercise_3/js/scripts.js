39fa8e3afb4cc2c5a7ae5d8db6be6ab68e5a0284
RodStud <rodion.novakovskiy@stu.shenkar.ac.il>
2024-06-18 14:40:23 +0300

    Exercise 3 - Added container for squares

--- Exercise_3/js/scripts.js
+++ Exercise_3/js/scripts.js
@@ -1,23 +1,52 @@
+let person_name = "RodionNovakovskiy";
+let colors = ["MediumSpringGreen", "MediumVioletRed", "MediumTurquoise", "MediumPurple"];
+let colors_index = 0;
+let name_index = 0;
+
 window.onload = () => {
-}
+  initRectangles();
+};
 
 function initRectangles() {
+  for (i = 0; i < person_name.length; i++) {
+    addRectangle();
+  }
 }
 
-function initSongs() {
-}
+function initSongs() {}
 
-function chooseRectangleColor() {
-}
+function chooseRectangleColor() {}
 
 function addRectangle() {
+  let squares_container = document.getElementById("squares-container");
+  let new_square = `<section class="square" style="background-color: ${colors[colors_index]}">${person_name[name_index]}</section>`;
+  squares_container.innerHTML += new_square;
+  nameIndexChange(1);
+  colorIndexChange(1);
 }
 
-function subtractRectangle() {
+function subtractRectangle() {}
+
+function switchRectanglesSongs() {}
+
+function populateSongsInList() {}
+
+function nameIndexChange(number) {
+  if (name_index + number >= person_name.length) {
+    name_index = 0;
+  } else if (name_index + number < 0) {
+    name_index = person_name.length - 1;
+  } else {
+    name_index += number;
+  }
 }
 
-function switchRectanglesSongs() {
-}
-
-function populateSongsInList() {
+function colorIndexChange(number) {
+  if (colors_index + number >= colors.length) {
+    colors_index = 0;
+  } else if (colors_index + number < 0) {
+    colors_index = colors.length - 1;
+  } else {
+    colors_index += number;
+  }
 }
